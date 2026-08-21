03ca570efe9e04fd49e693e09247560047877eb6
riya rathi <riyarathi2211@gmail.com>
2026-08-21 08:46:44 +0530

    Remove temporary DNS overrides from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+// import dns from 'dns';
+// dns.setDefaultResultOrder('ipv4first');
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
